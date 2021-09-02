f12bf5db61af40b9a9a90fdfa6fef7897178cad2
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-09-02 10:03:43 +0100

    add Story map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TinderCard from "react-tinder-card";
+import { useState } from "react";
+
+const storyMap = [
+  {
+    text: "You arrive at a crossroad.",
+    option1: { text: "Go Left", leadsTo: 1 },
+    option2: { text: "Go Right", leadsTo: 2 },
+  },
+  {
+    text: "You see a chest on the ground",
+    option1: { text: "Open it", leadsTo: 3 },
+    option2: { text: "Go Back", leadsTo: 0 },
+  },
+  {
+    text: "A monster eats you. The end.",
+    option1: { text: "Try again", leadsTo: 0 },
+  },
+  {
+    text: "You found the treasure. The end.",
+    option1: { text: "Try again", leadsTo: 0 },
+  },
+];
 
 function App() {
+  const onSwipe = (direction) => {
+    console.log("You swiped: " + direction);
+  };
+
+  const onCardLeftScreen = (myIdentifier) => {
+    console.log(myIdentifier + " left the screen");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TinderCard
+        className="card"
+        onSwipe={onSwipe}
+        onCardLeftScreen={() => onCardLeftScreen("fooBar")}
+        preventSwipe={["right", "left"]}
+      >
+        Hello, World!
+      </TinderCard>
     </div>
   );
 }
